fbc14f28962081ffa5062cd31220e5a68c5cfdf6
Shourya <shouryagaur101@gmail.com>
2025-08-27 21:55:48 +0530

    Update ArrayInput.java

--- Java/ArrayInput.java
+++ Java/ArrayInput.java
@@ -3,7 +3,13 @@ import java.util.Arrays;
 public class ArrayInput {
 
     static void printArray(int arr[]){
-        
+        for (int i = 0; i < arr.length; i++){
+       //     System.out.println(arr[i]);
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
 
